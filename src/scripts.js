99b76abf43820e43af807fb337930d22635f0be7
hemuba <125600353+hemuba@users.noreply.github.com>
2025-09-30 15:53:18 +0300

    try on BE <-> FE commuication

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,27 @@
-let questionId = document.getElementById("question_id")
-let answerId = document.getElementById("answer_id")
-let skillsId = document.getElementById("skills_id")
+const listEl = document.getElementById("stats-list");
+const btn = document.getElementById("load-btn");
+const m_status = document.getElementById("status")
 
-answerId.addEventListener("keydown", function(event){
-    if (event.key === "Enter") {
-        let answVar = answerId.value.trim()
-        if(!answVar) return;
-        
-        const li = document.createElement("li")
-        li.textContent = answVar;
-        skillsId.appendChild(li)
-        console.log(answVar)
-        answerId.value = "";
+btn.addEventListener("click", async () => {
+  try {
+    btn.disabled = true;
+    m_status.textContent = "Loading... "
+    const res = await fetch("http://127.0.0.1:8000/stats", { method: "GET" });
+    if (!res.ok) throw new Error("HTTP " + res.status);
+    const data = await res.json();
+
+    listEl.innerHTML = "";
+    for (const [k, v] of Object.entries(data)) {
+      const li = document.createElement("li");
+      li.textContent = `${k}: ${v}`;
+      listEl.appendChild(li);
     }
-}) 
+    m_status.textContent = "Done."
+  } catch (err) {
+    m_status.textContent= "Error loading stats."
+    console.error(err);
+  }finally {
+    btn.disabled = false;
+  }
+  
+});
